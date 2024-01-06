6d60ce536997419f3d855b2db59b02b4c328bd67
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-01-06 10:13:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
+	var boldElements = document.querySelectorAll('strong');
 
-
+  // Change the color of each bold element to green
+  boldElements.forEach(function (boldElement) {
+    boldElement.style.color = 'green';
+  });
 }
 
 
 function return_normal() {
     //Write your code here
+	var boldElements = document.querySelectorAll('strong');
 
+  // Change the color of each bold element back to black
+  boldElements.forEach(function (boldElement) {
+    boldElement.style.color = 'black';
+  });
     
 }
